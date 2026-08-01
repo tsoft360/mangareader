c8f6d603df28f6300979fc6b053dc9b508369054
tsoft360 <thijsboelens2@gmail.com>
2026-08-01 12:51:08 +0200

    started on the logic

---
+++ src/reader/folder_reader.rs
@@ -0,0 +1,41 @@
+use std::path::PathBuf;
+
+pub struct Chapter {
+    pub name: String,
+    pub pages: Vec<PathBuf>,
+}
+
+impl Chapter {
+    pub fn load(&self) {
+        
+    }
+}
+
+pub struct Manga {
+    pub title: String,
+    pub chapters: Vec<Chapter>,
+}
+
+impl Manga {
+    pub fn load(&self) {
+
+    }
+}
+
+pub struct FolderReader {
+    pub manga: Manga,
+
+    pub current_chapter: usize,
+    pub current_page: usize,
+}
+
+impl FolderReader {
+    pub fn current_page_path(&self) -> Option<&Path> {
+        self.manga
+            .chapters
+            .get(self.current_chapter)?
+            .pages
+            .get(self.current_page)
+            .map(|p| p.as_path())
+    }
+}
