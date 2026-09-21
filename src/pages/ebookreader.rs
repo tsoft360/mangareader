e922261dc1ac6c16cb19b7ca1cea4a6522950a48
tsoft360 <thijsboelens2@gmail.com>
2026-09-21 18:31:52 +0200

    how am i gonna parse xhtml (my english teacher said 'no using gonna' but welp did it anyway)

--- src/pages/ebookreader.rs
+++ src/pages/ebookreader.rs
@@ -3,7 +3,17 @@ use app::{MangaApp, Screen};
 use eframe::egui::Context;
 
 pub fn show(ctx: &Context, app: &mut MangaApp) {
-    let reader = &app.epub_reader;
+    let reader = &mut app.epub_reader;
+
+    let scroll = ctx.input(|i| i.raw_scroll_delta.y);
+
+    if scroll < 0.0 {
+        reader.next_chapter();
+    }
+
+    if scroll > 0.0 {
+        reader.previous_chapter();
+    }
 
     egui::TopBottomPanel::top("epub_reader_toolbar").show(ctx, |ui| {
         if ui.button("< Home").clicked() {
@@ -12,5 +22,9 @@ pub fn show(ctx: &Context, app: &mut MangaApp) {
         }
 
         ui.separator();
+    });
+
+    egui::CentralPanel::default().show(ctx, |ui| {
+        ui.label(reader.chapter_content.clone());
     });
 }
